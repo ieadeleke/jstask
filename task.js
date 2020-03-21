d4ce1db5b3c2c4b1ff88cd4100b386b2a037af4b
ieadeleke <58973732+ieadeleke@users.noreply.github.com>
2020-03-21 16:23:13 +0000

    Update task.js

--- task.js
+++ task.js
@@ -5,6 +5,10 @@ var courses = ['HTML','CSS','Javascript','PHP','Node.js'];
 
 console.log(name)
 
+for ( i = 0; i < courses.length; i++ ) {
+  console.log(courses[i])
+}
+
 
 for ( i = 0; i <= 200; i++ ) {
   if ( i % 2 !== 0 ) {
