fa413385030cd5e0fa19d3c4797f5b42ce60ed9d
ieadeleke <58973732+ieadeleke@users.noreply.github.com>
2020-03-21 16:18:48 +0000

    Update and rename README.md to task.js

---
+++ task.js
@@ -0,0 +1,15 @@
+var name = 'ieadeleke';
+
+var courses = ['HTML','CSS','Javascript','PHP','Node.js'];
+
+
+console.log(name)
+
+
+for ( i = 0; i <= 200; i++ ) {
+  if ( i % 2 !== 0 ) {
+    console.log(i)
+    };
+}
+
+
